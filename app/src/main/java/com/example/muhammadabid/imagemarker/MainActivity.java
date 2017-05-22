117b8486e8ce097bc6eeae900b400aad4a958cbf
MuhammadAbid <abid58b@gmail.com>
2017-05-22 12:42:59 +0500

    Test

---
+++ app/src/main/java/com/example/muhammadabid/imagemarker/MainActivity.java
@@ -0,0 +1,293 @@
+package com.example.muhammadabid.imagemarker;
+
+import android.*;
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Geocoder;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telecom.Connection;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.GooglePlayServicesAvailabilityException;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.identity.intents.Address;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.nearby.connection.Connections;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.jar.*;
+
+public abstract class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    GoogleMap mGoogleMap;
+    GoogleApiClient mGoogleApiClient;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView image;
+    Bitmap imageBitmap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.image=(ImageView) this.findViewById(R.id.imageView1);
+        if (googleServicesAvailable()) {
+            Toast.makeText(this, "Perfect!!!", Toast.LENGTH_LONG).show();
+            setContentView(R.layout.activity_main);
+
+            initMap();
+        } else {
+            //no Google maps layout
+
+        }
+    }
+    private void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    private boolean googleServicesAvailable() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS)
+            return true;
+        else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "can not connect to play services", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        goTOLocationZoom(33.655998, 73.0220413, 15);
+        if(mGoogleMap!=null){
+
+            mGoogleMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+                @Override
+                public void onMarkerDragStart(Marker marker) {
+
+                }
+
+                @Override
+                public void onMarkerDrag(Marker marker) {
+
+                }
+
+                @Override
+                public void onMarkerDragEnd(Marker marker) {
+                    Geocoder gc=new Geocoder(MainActivity.this);
+                    LatLng ll=marker.getPosition();
+                    double lat=ll.latitude;
+                    double lng=ll.longitude;
+                    List<android.location.Address> list=null;
+                    try {
+                        list=gc.getFromLocation(lat,lng,1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    android.location.Address add=list.get(0);
+                    marker.setTitle(add.getLocality());
+                    marker.showInfoWindow();
+
+                }
+            });
+            mGoogleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                @Override
+                public View getInfoWindow(Marker marker) {
+                    return null;
+                }
+
+                @Override
+                public View getInfoContents(Marker marker) {
+                    View v=getLayoutInflater().inflate(R.layout.info_window,null);
+
+                    TextView tvLocality=(TextView) v.findViewById(R.id.tv_locality);
+                    TextView tvLat=(TextView) v.findViewById(R.id.tv_lat);
+                    TextView tvLng=(TextView) v.findViewById(R.id.tv_lng);
+                    TextView tvSnippet=(TextView) v.findViewById(R.id.tv_snippet);
+                    ImageView image=(ImageView) v.findViewById(R.id.imageView1);
+
+                    LatLng ll=marker.getPosition();
+                    tvLocality.setText(marker.getTitle());
+                    tvLat.setText("Latitude"+ ll.latitude);
+                    tvLng.setText("Longitude"+ ll.longitude);
+                    tvSnippet.setText(marker.getSnippet());
+                    image.setImageBitmap(imageBitmap);
+
+                    return v;
+                }
+            });
+        }
+    }
+
+    private void goTOLocation(double lat, double lng) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
+        mGoogleMap.moveCamera(update);
+    }
+
+    private void goTOLocationZoom(double lat, double lng, float zoom) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
+        mGoogleMap.moveCamera(update);
+    }
+    Marker marker;
+
+    public void geoLocate(View view) throws IOException {
+        EditText et = (EditText) findViewById(R.id.editText);
+        String location = et.getText().toString();
+        Geocoder gc = new Geocoder(this);
+        List<android.location.Address> list = gc.getFromLocationName(location, 1);
+        android.location.Address address = list.get(0);
+        String locality = address.getLocality();
+        Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
+
+        double lat = address.getLatitude();
+        double lng = address.getLongitude();
+        goTOLocationZoom(lat, lng, 15);
+        setMarker(locality, lat, lng);
+    }
+
+    private void setMarker(String locality, double lat, double lng) {
+//        if(marker==null){
+//            marker.remove();
+//        }
+
+        MarkerOptions options=new MarkerOptions()
+                .title(locality)
+                .draggable(true)
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
+                .position(new LatLng(lat,lng))
+                .snippet("i am here");
+        marker= mGoogleMap.addMarker(options);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.maps, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mapTypeNone:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+            case R.id.mapTypeNormal:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.mapTypeTerrain:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.mapTypeSatellite:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.mapTypeHybrid:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    LocationRequest mLocationRequest;
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location==null)
+        {
+            Toast.makeText(this, "Can not get current location", Toast.LENGTH_LONG).show();
+        }
+        else {
+            LatLng ll=new LatLng(location.getLatitude(),location.getLongitude());
+            CameraUpdate update=CameraUpdateFactory.newLatLngZoom(ll,15);
+            mGoogleMap.animateCamera(update);
+        }
+    }
+    public void dispatchTakePictureIntent(View v) {
+        if (v.getId() == R.id.pic) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI.getPath());
+            startActivityForResult(intent, 1);
+            //onActivityResult(1,1,intent);
+        }
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+
+            //image.setImageBitmap(imageBitmap);
+        }
+    }
+}
